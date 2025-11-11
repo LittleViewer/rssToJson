2ba1dbe050af99cb555ed156d36b96722012d7d7
LittleViewer <planete.adoecrivain@gmail.com>
2025-11-11 20:42:58 +0100

    Add visual separators to script output
    Added print statements with slashes before and after the main output to improve readability of the script's console output.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import libCore.feed_class as feed
 import datetime
+print("//////////////////////////")
 date_ = datetime.datetime.now()
 feed_ = feed.feed()
 
 count_number_article = feed_.pipe_extract_rss()
 
 print(f"[{date_}] - Run complete with {count_number_article} new article !")
+print("//////////////////////////")
